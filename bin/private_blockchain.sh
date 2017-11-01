7ef994fa3be71b7ae533d8d978752cd65b17d440
miss61008596 <61008596@qq.com>
2017-11-01 17:07:20 +0800

    edit

--- bin/private_blockchain.sh
+++ bin/private_blockchain.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 geth=${GETH:-geth}
-
-$geth --datadir data --networkid 31415926 --rpc --rpccorsdomain "*" --nodiscover console
+nohup $geth --unlock 0 --password ./bin/password --datadir data --networkid 61008596 --ws --wsport 4046 --wsorigins '*' --rpc --rpcaddr "0.0.0.0" --rpcport 4045 --rpcapi="db,eth,net,web3,personal,web3,miner,debug" --rpccorsdomain '*' --nodiscover &
--- bin/private_blockchain_init.sh
+++ bin/private_blockchain_init.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 geth=${GETH:-geth}
-
-$geth --datadir data --networkid 31415926 --rpc --rpccorsdomain "*" init ./genesis.json
+$geth --datadir data --networkid 61008596 --rpc --rpccorsdomain "*" init ./genesis.json
